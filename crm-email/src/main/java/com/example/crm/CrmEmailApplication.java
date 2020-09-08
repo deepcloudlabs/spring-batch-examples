cb9aaff21de03d064cf94d00c18df68b2b599a26
deepcloudlabs <deepcloudlabs@gmail.com>
2020-09-08 11:08:40 +0300

    add config

---
+++ crm-email/src/main/java/com/example/crm/CrmEmailApplication.java
@@ -0,0 +1,13 @@
+package com.example.crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmEmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrmEmailApplication.class, args);
+	}
+
+}
